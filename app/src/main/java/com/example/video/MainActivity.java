47f38fecb3ec598e93e68ca1d6eb8c82d35db795
RominaNavarrete <rominagnavarrete@gmail.com>
2023-10-25 20:43:30 -0300

    Initial commit

---
+++ app/src/main/java/com/example/video/MainActivity.java
@@ -0,0 +1,26 @@
+package com.example.video;
+
+import androidx.appcompat.app.AppCompatActivity;
+import android.os.Bundle;
+import android.net.Uri; // Importa la clase Uri
+import android.widget.MediaController;
+import android.widget.VideoView;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        VideoView videoView = findViewById(R.id.video_view);
+        String videoPath = "android.resource://" + getPackageName()  + "/" + R.raw.video;
+        Uri uri = Uri.parse(videoPath);
+        videoView.setVideoURI(uri);
+
+        MediaController mediaController = new MediaController(this);
+        videoView.setMediaController(mediaController);
+        mediaController.setAnchorView(videoView);
+
+    }
+}
